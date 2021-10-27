e12af9233d965af2c62d6ee8b0541eaf77803391
BearLikeCode <bearlikecode@gmail.com>
2021-10-28 02:32:02 +0300

    add env, translator

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+'use strict';
+
+require('dotenv').config()
 const Bot = require('node-telegram-bot-api');
-const API_KEY = '2073969027:AAH2pcMYkHYpxBYI9cyBRLP9du_Gmq-01NI';
-const bot = new Bot(API_KEY, { polling: true });
+const { TranslationServiceClient } = require('@google-cloud/translate');
+
+console.log(process.env.API_KEY);
+
+const bot = new Bot(process.env.API_KEY, { polling: true });
+const translationClient = new TranslationServiceClient();
+
+async function translateText(text) {
+    console.log(text);
+    const request = {
+        parent: `projects/${process.env.PROJECT_ID}/locations/${process.env.LOCATION}`,
+        contents: [text],
+        mimeType: 'text/plain', // mime types: text/plain, text/html
+        sourceLanguageCode: 'ru',
+        targetLanguageCode: 'en',
+    };
+
+    const [response] = await translationClient.translateText(request);
+  
+    for (const translation of response.translations) {
+        console.log(`Translation: ${translation.translatedText}`);
+    }
+}
 
 bot.onText(/\/print (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
+    translateText(match[1])
     const response = match[1];
 
     bot.sendMessage(chatId, response);
